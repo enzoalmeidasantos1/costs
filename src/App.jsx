a146ea465bb2679038d94d4267638e5eb1bff107
Emerson Almeida <Emersoncopywriter21@gmail.com>
2026-01-21 15:11:10 -0300

    primeiro arquivo

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layouts/Container";
+import Navbar from "./components/layouts/Navbar"
+import Footer from "./components/layouts/Footer"
+import Projects from "./components/pages/Projects";
+
+function App() {
+  // npx json-server --watch db.json --port 5000 //
+  return (
+    <Router>
+        <Navbar/>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
